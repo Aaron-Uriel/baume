1b9b1b31a32ee21e99beb85496e7eb08799cd37b
ferfabianreyes <2100721a@umich.mx>
2023-05-13 15:08:25 -0600

    Imprimir menú principal

--- src/main.c
+++ src/main.c
@@ -3,10 +3,34 @@
 int
 main(int argc, char *argv[])
 {
+    int option;
     if (argc > 1) {
         printf("Hola, recibí argumentos\n");
     }
 
-    printf("Hola xd.\n");
+    option =  menu();
+
     return 0;
+}
+
+
+int menu()
+{
+    int option;
+    printf("═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬\n\t"
+            "x x ⁞ Menú. x x\n\t"
+            "═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬\n\t"
+            "▒┊1.- Agregar. (Crear + Insertar)\n\t"
+            "▒┊2.- Quitar. (Extraer + eliminar)\n\t"
+            "▒┊3.- Imprimir.\n\t"
+            "▒┊4.- Preorden\n\t"
+            "▒┊5.- Posorden.\n\t"
+            "▒┊6.- Enorden.\n\t"
+            "▒┊7.- Buscar.\n\t"
+            "▒┊8.- Altura.\n\t"
+            "▒┊9.- Salir.\n\t"
+            "═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬═▬\n\t"
+            "➫ ");
+    scanf("%d", &option);
+    return option;
 }
