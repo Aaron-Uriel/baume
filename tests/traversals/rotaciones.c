5900a805c7b7d6cd1a4da719b81a5fe039c5f352
FerFabianReyes <2100721a@umich.mx>
2023-05-28 15:35:48 -0600

    Archivo de rotaciones, estructura prueba AVL y rotación II

---
+++ tests/traversals/rotaciones.c
@@ -0,0 +1,36 @@
+#include <baume.h>
+
+/*Estructura de prueba para rotaciones*/
+typedef struct NodeAVL NodeAVL;
+struct NodeAVL {
+	int  value;
+    int fe;
+	NodeAVL *right;
+    NodeAVL *left;
+};
+
+/*Prototipos de prueba para rotaciones*/
+void RotationLeftLeft(NodeAVL **problem, NodeAVL *next);
+
+
+
+/*Rotación simple izquierda izquierda*/
+void RotationLeftLeft(NodeAVL **problem, NodeAVL *next) //Nodo con fe mayor, el que le sigue
+{
+    (*problem)->left = next->right;
+    next->right = (*problem);
+
+    //Cambio del fe
+    if (next->fe == -1)
+    {
+        (*problem)->fe = 0;
+        next->fe = 0;
+    } else
+    {
+        (*problem)->fe = -1;
+        next->fe = 1;
+    }
+
+    (*problem) = next;
+    
+}
