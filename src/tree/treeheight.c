63dfc12ce332bc4885184348635e056679b46bbd
EverardoBedolla <2100553j@umich.mx>
2023-05-29 07:34:24 -0600

    Corrección de línea

--- src/tree/treeheight.c
+++ src/tree/treeheight.c
@@ -5,7 +5,7 @@ int treeheigth (int a [], int n){
     int vez, treelevel;
     for (vez = 0; vez < n; vez++){
         if (a[vez] -> a[vez + 1]){
-            ++treelevel:
+            ++treelevel;
         }
     }
     return treelevel;
