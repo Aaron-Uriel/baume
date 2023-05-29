468af3d7107b8309b72ad1ddf332a6cb3e83b645
EverardoBedolla <2100553j@umich.mx>
2023-05-29 07:33:19 -0600

    Registro y devolución

--- src/tree/treeheight.c
+++ src/tree/treeheight.c
@@ -5,7 +5,8 @@ int treeheigth (int a [], int n){
     int vez, treelevel;
     for (vez = 0; vez < n; vez++){
         if (a[vez] -> a[vez + 1]){
-            
+            ++treelevel:
         }
     }
+    return treelevel;
 }
