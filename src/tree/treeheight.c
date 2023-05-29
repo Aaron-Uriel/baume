aae2de943bc84444b40ff2f539429703387031dc
EverardoBedolla <2100553j@umich.mx>
2023-05-29 08:01:51 -0600

    Creación de inserction

--- src/tree/treeheight.c
+++ src/tree/treeheight.c
@@ -4,7 +4,7 @@
 int treeheigth (int a [], int n){
     int vez, treelevel;
     for (vez = 0; vez < n; vez++){
-        if (a [vez] -> a[vez + 1] && a [vez + 1] -> a [vez]){
+        if (a [vez] -> a[vez + 1]){
             ++treelevel;
         }
         else {
